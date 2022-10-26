e174113946832bfc3f562f50cdc1cb89e6a6425a
yogastama <yogabagas69@gmail.com>
2022-10-26 09:55:23 +0700

    UPD

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
     public function index(Request $request)
     {
         if (!auth()->check()) {
-            return redirect()->to('/login');
+            return redirect()->to('/admin/login');
         }
         $event = EventModel::where('is_active', 'active');
         if ($request->get('event_id')) {
